9f15c8abe26991fb97a63b977ca5aac4db832d77
Rushil <rushil.ramautar@gmail.com>
2023-01-10 13:34:58 -0300

    added api endpoints for actions and analytics

--- app/Http/Controllers/SmsController.php
+++ app/Http/Controllers/SmsController.php
@@ -2,9 +2,44 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\BulkSendSmsLaterRequest;
+use App\Http\Requests\BulkSendSmsNowRequest;
+use App\Http\Requests\SendSmsLaterRequest;
+use App\Http\Requests\SendSmsNowRequest;
 use Illuminate\Http\Request;
 
 class SmsController extends Controller
 {
-    //
+    public function sendNow(SendSmsNowRequest $request)
+    {
+        // recipient: 597(7digits)
+        // body: required|string|min:1|max:160
+    }
+
+    public function sendLater(SendSmsLaterRequest $request)
+    {
+        // recipient: required|597(7digits)
+        // body: required|string|min:1|max:160
+        // datetime: required|datetime|laterthannow
+
+    }
+
+    public function cancelSms($smsUuid, Request $request)
+    {}
+
+    public function bulkSendNow(BulkSendSmsNowRequest $request)
+    {
+        // recipients: required|array|597(7digits)
+        // body: required|min:1|max:160
+    }
+
+    public function bulkSendLater(BulkSendSmsLaterRequest $request)
+    {
+        // recipients: required|array|597(7digits)
+        // body: required|min:1|max:160
+        // datetime: required|datetime|laterthannow
+    }
+
+    public function cancelBulk($bulkUuid, Request $request)
+    {}
 }
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\HistoryController;
+use App\Http\Controllers\SmsController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -15,8 +17,23 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/me', function (Request $request) {
-    return $request->user();
+Route::middleware('auth:sanctum')->group(function() {
+    Route::get('/me', [AuthController::class, 'me'])->name('me');
+    Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+
+    Route::prefix('/sms')->group(function() {
+        Route::post('/send/now', [SmsController::class, 'sendNow'])->name('sms.sendnow');
+        Route::post('/send/later', [SmsController::class, 'sendLater'])->name('sms.sendlater');
+        Route::post('/cancel/{smsUuid}', [SmsController::class, 'sendLater'])->name('sms.sendlater');
+        Route::post('/bulksend/now', [SmsController::class, 'bulkSendNow'])->name('sms.bulksendnow');
+        Route::post('/bulksend/later', [SmsController::class, 'bulkSendLater'])->name('sms.bulksendlater');
+    });
+
+    Route::prefix('/history')->group(function() {
+        Route::get('/', [HistoryController::class, 'index'])->name('history.index');
+        Route::get('/{smsUuid}', [HistoryController::class, 'show'])->name('history.show');
+        Route::get('/{smsUuid}/isSent', [HistoryController::class, 'isSent'])->name('history.isSent');
+    });
 });
 
 Route::post('/login', [AuthController::class, 'login'])->name('login');
