af12e2d2a6633aecc33033443e7e34c32af100cd
Rushil <rushil.ramautar@gmail.com>
2023-11-07 13:19:23 -0300

    added number carrier verification helper

--- app/helpers.php
+++ app/helpers.php
@@ -9,3 +9,41 @@ function ordinalize(int $number)
     }
     return $number . $suffix;
 }
+
+function isDigicelNumber(int $phoneNumber)
+{
+    if ($phoneNumber >= 7100000 && $phoneNumber <= 7299999) {
+        return true;
+    }
+
+    if ($phoneNumber >= 7400000 && $phoneNumber <= 7499999) {
+        return true;
+    }
+
+    if ($phoneNumber >= 7600000 && $phoneNumber <= 7699999) {
+        return true;
+    }
+
+    if ($phoneNumber >= 8100000 && $phoneNumber <= 8299999) {
+        return true;
+    }
+
+    return false;
+}
+
+function isTelesurNumber(int $phoneNumber)
+{
+    if ($phoneNumber >= 7500000 && $phoneNumber <= 7599999) {
+        return true;
+    }
+
+    if ($phoneNumber >= 7700000 && $phoneNumber <= 7799999) {
+        return true;
+    }
+
+    if ($phoneNumber >= 8400000 && $phoneNumber <= 8999999) {
+        return true;
+    }
+
+    return false;
+}
