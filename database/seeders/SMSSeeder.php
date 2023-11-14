9d5d9cd02df86b51136308cc1ae6f2ad6de0f5ea
Rushil <rushil.ramautar@gmail.com>
2023-11-14 14:58:33 -0300

    sms seeder only seeding in local

--- database/seeders/SMSSeeder.php
+++ database/seeders/SMSSeeder.php
@@ -20,53 +20,55 @@ class SMSSeeder extends Seeder
      */
     public function run()
     {
-        $faker = Faker::create();
-        $types = ['instant', 'scheduled'];
-        for ($i=0; $i < 10; $i++) {
-            $typesIndex = array_rand($types);
-            $type = $types[$typesIndex];
-            $bulk = $faker->boolean();
-            $message = $faker->text(140);
-            $job = SendJob::create([
-                'type' => $type,
-                'bulk' => $bulk,
-                'message' => $message,
-                'scheduled_at' => $type == 'scheduled' ? Carbon::now()->addMinutes(rand(30, 3600)) : null,
-            ]);
-            if ($bulk)
-            {
-                for ($j=0; $j < 50; $j++) {
+        if (env("APP_ENV") == 'local') {
+            $faker = Faker::create();
+            $types = ['instant', 'scheduled'];
+            for ($i=0; $i < 10; $i++) {
+                $typesIndex = array_rand($types);
+                $type = $types[$typesIndex];
+                $bulk = $faker->boolean();
+                $message = $faker->text(140);
+                $job = SendJob::create([
+                    'type' => $type,
+                    'bulk' => $bulk,
+                    'message' => $message,
+                    'scheduled_at' => $type == 'scheduled' ? Carbon::now()->addMinutes(rand(30, 3600)) : null,
+                ]);
+                if ($bulk)
+                {
+                    for ($j=0; $j < 50; $j++) {
+                        SMSMessage::create([
+                            'job_id' => $job->id,
+                            'recipient' => $faker->numerify('597#######'),
+                            'message' => $message,
+                        ]);
+                    }
+                } else {
                     SMSMessage::create([
                         'job_id' => $job->id,
                         'recipient' => $faker->numerify('597#######'),
                         'message' => $message,
                     ]);
                 }
-            } else {
-                SMSMessage::create([
-                    'job_id' => $job->id,
-                    'recipient' => $faker->numerify('597#######'),
-                    'message' => $message,
-                ]);
-            }
-            foreach($job->messages as $smsMessage)
-            {
-                $smsMessage = SMSMessage::where('id', $smsMessage->id)
-                    ->with(['jobs'])
-                    ->first();
-                $randomUser = User::inRandomOrder()->first();
+                foreach($job->messages as $smsMessage)
+                {
+                    $smsMessage = SMSMessage::where('id', $smsMessage->id)
+                        ->with(['jobs'])
+                        ->first();
+                    $randomUser = User::inRandomOrder()->first();
 
-                if($job->type == 'instant'){
-                    SendSms::dispatch($smsMessage, $randomUser);
-                } else if ($job->type == 'scheduled') {
-                    $scheduled_at = Carbon::parse($job->scheduled_at);
-                    if (Carbon::now()->greaterThanOrEqualTo($scheduled_at)) {
+                    if($job->type == 'instant'){
                         SendSms::dispatch($smsMessage, $randomUser);
-                    } else {
-                        $difference = Carbon::now()->diffInSeconds($scheduled_at);
-                        SendSms::dispatch($smsMessage, $randomUser)->delay(now()->addSeconds($difference));
-                    }
+                    } else if ($job->type == 'scheduled') {
+                        $scheduled_at = Carbon::parse($job->scheduled_at);
+                        if (Carbon::now()->greaterThanOrEqualTo($scheduled_at)) {
+                            SendSms::dispatch($smsMessage, $randomUser);
+                        } else {
+                            $difference = Carbon::now()->diffInSeconds($scheduled_at);
+                            SendSms::dispatch($smsMessage, $randomUser)->delay(now()->addSeconds($difference));
+                        }
 
+                    }
                 }
             }
         }
